230359faab4bafcae34e20507506aef58c68225f
Steve Thompson <writetome51@gmail.com>
2020-07-05 23:08:33 -0600

    Updated comments

--- lib/index.ts
+++ lib/index.ts
@@ -8,7 +8,11 @@ import { PaginationPageInfo } from './pagination-page-info/lib';
 
 
 /***************************
- BigDatasetPaginator is intended for a real-world web application.
+ BigDatasetPaginator is intended for pagination where all the data to be
+ paginated can't be loaded in memory at once. Instead of only requesting
+ one page of data at-a-time from the data source, the paginator has the
+ option of requesting a bigger load, determined by the function
+ `setItemsPerLoad()`.
  In the constructor you pass in a `dataSource` that returns data in loads.  Each load can
  contain multiple pages worth of data.  (A load is either the total amount of data you want the
  app to have loaded in memory at once, or the total amount of data the data source is willing to
